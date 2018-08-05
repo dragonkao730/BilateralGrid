44040fdf66028045ff711adc938d2e4ab97c3225
dragonkao730 <r05922122@ntu.edu.tw>
2018-08-05 19:02:40 +0800

    Update bilaterial_grid.py

--- bilaterial_grid.py
+++ bilaterial_grid.py
@@ -3,7 +3,6 @@ import tensorflow as tf
 def get_tensor_shape(x):
     a = x.get_shape().as_list()
     b = [tf.shape(x)[i] for i in range(len(a))]
-    r = []
     return [aa if type(aa) is int else bb for aa, bb in zip(a, b)]
 
 # [b, n, c]
