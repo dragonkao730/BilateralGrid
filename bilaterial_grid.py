69cdb6af3b1fc1910a00c8086b34f8911f7ab83b
dragonkao730 <r05922122@ntu.edu.tw>
2018-08-05 18:08:47 +0800

    Update bilaterial_grid.py

--- bilaterial_grid.py
+++ bilaterial_grid.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 
-
 def get_tensor_shape(x):
     a = x.get_shape().as_list()
     b = [tf.shape(x)[i] for i in range(len(a))]
@@ -8,3 +7,46 @@ def get_tensor_shape(x):
     for aa, bb in zip(a, b):
         r.append(aa if type(aa) is int else bb)
     return r
+
+# [b, n, c]
+def sample_1d(
+    img,   # [b, h, c]
+    y_idx, # [b, n], 0 <= pos < h, dtpye=int32
+):
+    b, h, c = get_tensor_shape(img)
+    b, n    = get_tensor_shape(y_idx)
+    
+    b_idx = tf.range(b, dtype=tf.int32) # [b]
+    b_idx = tf.expand_dims(b_idx, -1)   # [b, 1]
+    b_idx = tf.tile(b_idx, [1, n])      # [b, n]
+    
+    y_idx = tf.clip_by_value(y_idx, 0, h - 1) # [b, n]
+    a_idx = tf.stack([b_idx, y_idx], axis=-1) # [b, n, 2]
+    
+    return tf.gather_nd(img, a_idx)
+
+# [b, n, c]
+def interp_1d(
+    img, # [b, h, c]
+    y,   # [b, n], 0 <= pos < h, dtype=float32
+):
+    b, h, c = get_tensor_shape(img)
+    b, n    = get_tensor_shape(y)
+    
+    y_0 = tf.floor(y) # [b, n]
+    y_1 = y_0 + 1    
+    
+    _sample_func = lambda y_x: sample_1d(
+        img,
+        tf.cast(y_x, tf.int32)
+    )
+    y_0_val = _sample_func(y_0) # [b, n, c]
+    y_1_val = _sample_func(y_1)
+    
+    w_0 = y_1 - y # [b, n]
+    w_1 = y - y_0
+    
+    w_0 = tf.expand_dims(w_0, -1) # [b, n, 1]
+    w_1 = tf.expand_dims(w_1, -1)
+    
+    return w_0*y_0_val + w_1*y_1_val
